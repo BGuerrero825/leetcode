6a6fb33446f3bcb35a32696db21cc3024dd2fe9b
babu <bguerrero825@gmail.com>
2025-05-20 14:47:22 -0500

    linked list creation function and some explanation of stuff for dummies like me

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,7 +1,10 @@
 mod nth_node_from_end;
+use nth_node_from_end::Solution as Sol;
 
 fn main() {
-    let res = "placeholder" ;
+    let list = Sol::create_list(10);
+    println!("List: {list:?}");
+    let res = Sol::remove_nth_from_end(list, 4) ;
     println!("Result: {res:?}");
     println!("Hello, world!");
 }
--- rust/src/nth_node_from_end.rs
+++ rust/src/nth_node_from_end.rs
@@ -19,8 +19,25 @@ impl ListNode {
 pub struct Solution {}
 
 impl Solution {
-    pub fn remove_nth_from_end(head: Option<Box<ListNode>>, n: i32) -> Option<Box<ListNode>> {
 
+    pub fn create_list(size: i32) -> Option<Box<ListNode>> {
+        let mut dummy = Box::new(ListNode::new(0));
+        let mut curr = &mut dummy;
+        for i in 0..size {
+            // curr is a ref to a mutable (Boxed) ListNode: change the referenced Node's 'next' field
+            // by setting it to a newly alloced (Optional + Boxed) ListNode
+            curr.next = Some(Box::new(ListNode::new(i)));
+            // curr is also itself mutable: change curr to reference 'next's (Opt + Boxed) ListNode
+            // BUT, curr needs a reference to a mutable Boxed ListNode, not a mutable REFERENCE to
+            // an Optional + Boxed ListNode. So, as_mut() adjusts the mutability target to the
+            // Boxed ListNode contained within the Optional, making the assignment valid.
+            curr = curr.next.as_mut()?;
+        }
+
+        dummy.next
+    }
+
+    pub fn remove_nth_from_end(head: Option<Box<ListNode>>, n: i32) -> Option<Box<ListNode>> {
         let mut dummy = Box::new(ListNode { val: 0, next: head });
         let mut len = 0;
         let mut curr = &dummy.next;
@@ -32,7 +49,7 @@ impl Solution {
         let pos = len - n;
         let mut curr = &mut dummy;
         for _ in 0..pos {
-            curr = curr.next.as_mut().unwrap();
+            curr = curr.next.as_mut()?;
         }
 
         if let Some(mut removee) = curr.next.take() {
